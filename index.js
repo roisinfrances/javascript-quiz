ac2996353b72cdbaceaa03f7856b0c6262ec2693
Roisin <90288595+roisinfrances@users.noreply.github.com>
2021-09-23 22:19:51 +0100

    updated results

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ function nextQuestion(thisQuestion, nextQuestion) {
     document.getElementById(thisQuestion).style.display = "none";
     document.getElementById(nextQuestion).style.display = "inline-block";
 
-    // can I add a function to this to validate whether a box is checked / not let it move on to next q if button is empty?? 
 }
+
 
 // function to calculate results???? // 
 
